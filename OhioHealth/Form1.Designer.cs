9bff2a860cef6d50765e1cf7d5fa81d79af58f06
award068 <56238935+award068@users.noreply.github.com>
2019-10-06 20:09:39 -0400

    Add files via upload

---
+++ OhioHealth/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿using System.Windows.Forms;
+
+namespace OhioHealth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OKBtn = new System.Windows.Forms.Button();
+            this.DateTimeStartTxtBox = new System.Windows.Forms.TextBox();
+            this.Copyright = new System.Windows.Forms.TextBox();
+            this.DateTimeEndTxtBox = new System.Windows.Forms.TextBox();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+            this.BedTimeTxtBox = new System.Windows.Forms.TextBox();
+            this.startTime = new System.Windows.Forms.DateTimePicker();
+            this.bedTime = new System.Windows.Forms.DateTimePicker();
+            this.endTime = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // OKBtn
+            // 
+            this.OKBtn.Location = new System.Drawing.Point(524, 136);
+            this.OKBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.OKBtn.Name = "OKBtn";
+            this.OKBtn.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.OKBtn.Size = new System.Drawing.Size(100, 28);
+            this.OKBtn.TabIndex = 6;
+            this.OKBtn.Text = "OK";
+            this.OKBtn.UseVisualStyleBackColor = true;
+            this.OKBtn.Click += new System.EventHandler(this.OKBtn_Click);
+            // 
+            // DateTimeStartTxtBox
+            // 
+            this.DateTimeStartTxtBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.DateTimeStartTxtBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DateTimeStartTxtBox.Location = new System.Drawing.Point(39, 15);
+            this.DateTimeStartTxtBox.Margin = new System.Windows.Forms.Padding(4);
+            this.DateTimeStartTxtBox.Name = "DateTimeStartTxtBox";
+            this.DateTimeStartTxtBox.Size = new System.Drawing.Size(133, 15);
+            this.DateTimeStartTxtBox.TabIndex = 1;
+            this.DateTimeStartTxtBox.TabStop = false;
+            this.DateTimeStartTxtBox.Text = "Date and Time Start:";
+            // 
+            // Copyright
+            // 
+            this.Copyright.BackColor = System.Drawing.SystemColors.Menu;
+            this.Copyright.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Copyright.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Copyright.Location = new System.Drawing.Point(13, 149);
+            this.Copyright.Margin = new System.Windows.Forms.Padding(4);
+            this.Copyright.Name = "Copyright";
+            this.Copyright.Size = new System.Drawing.Size(125, 15);
+            this.Copyright.TabIndex = 5;
+            this.Copyright.TabStop = false;
+            this.Copyright.Text = "© 2019 A.Ward";
+            // 
+            // DateTimeEndTxtBox
+            // 
+            this.DateTimeEndTxtBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.DateTimeEndTxtBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DateTimeEndTxtBox.Location = new System.Drawing.Point(39, 91);
+            this.DateTimeEndTxtBox.Margin = new System.Windows.Forms.Padding(4);
+            this.DateTimeEndTxtBox.Name = "DateTimeEndTxtBox";
+            this.DateTimeEndTxtBox.Size = new System.Drawing.Size(133, 15);
+            this.DateTimeEndTxtBox.TabIndex = 1;
+            this.DateTimeEndTxtBox.TabStop = false;
+            this.DateTimeEndTxtBox.Text = "Date and Time End:";
+            // 
+            // dtStart
+            // 
+            this.dtStart.CustomFormat = "ddddd, MMMM dd, yyyy, hh:mm tt";
+            this.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtStart.Location = new System.Drawing.Point(180, 12);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.Size = new System.Drawing.Size(267, 22);
+            this.dtStart.TabIndex = 1;
+            // 
+            // dtEnd
+            // 
+            this.dtEnd.CustomFormat = "ddddd, MMMM dd, yyyy, hh:mm tt";
+            this.dtEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtEnd.Location = new System.Drawing.Point(180, 91);
+            this.dtEnd.Name = "dtEnd";
+            this.dtEnd.Size = new System.Drawing.Size(267, 22);
+            this.dtEnd.TabIndex = 4;
+            // 
+            // BedTimeTxtBox
+            // 
+            this.BedTimeTxtBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.BedTimeTxtBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.BedTimeTxtBox.Location = new System.Drawing.Point(39, 51);
+            this.BedTimeTxtBox.Margin = new System.Windows.Forms.Padding(4);
+            this.BedTimeTxtBox.Name = "BedTimeTxtBox";
+            this.BedTimeTxtBox.Size = new System.Drawing.Size(133, 15);
+            this.BedTimeTxtBox.TabIndex = 1;
+            this.BedTimeTxtBox.TabStop = false;
+            this.BedTimeTxtBox.Text = "Bed Time:";
+            // 
+            // startTime
+            // 
+            this.startTime.CustomFormat = "hh:mm tt";
+            this.startTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.startTime.Location = new System.Drawing.Point(453, 12);
+            this.startTime.Name = "startTime";
+            this.startTime.ShowUpDown = true;
+            this.startTime.Size = new System.Drawing.Size(171, 22);
+            this.startTime.TabIndex = 2;
+            // 
+            // bedTime
+            // 
+            this.bedTime.CustomFormat = "hh:mm tt";
+            this.bedTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.bedTime.Location = new System.Drawing.Point(180, 51);
+            this.bedTime.Name = "bedTime";
+            this.bedTime.ShowUpDown = true;
+            this.bedTime.Size = new System.Drawing.Size(171, 22);
+            this.bedTime.TabIndex = 3;
+            // 
+            // endTime
+            // 
+            this.endTime.CustomFormat = "hh:mm tt";
+            this.endTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.endTime.Location = new System.Drawing.Point(453, 91);
+            this.endTime.Name = "endTime";
+            this.endTime.ShowUpDown = true;
+            this.endTime.Size = new System.Drawing.Size(171, 22);
+            this.endTime.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.OKBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 177);
+            this.Controls.Add(this.dtEnd);
+            this.Controls.Add(this.endTime);
+            this.Controls.Add(this.bedTime);
+            this.Controls.Add(this.startTime);
+            this.Controls.Add(this.dtStart);
+            this.Controls.Add(this.Copyright);
+            this.Controls.Add(this.DateTimeEndTxtBox);
+            this.Controls.Add(this.BedTimeTxtBox);
+            this.Controls.Add(this.DateTimeStartTxtBox);
+            this.Controls.Add(this.OKBtn);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "OhioHealth";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OKBtn;
+        private System.Windows.Forms.TextBox DateTimeStartTxtBox;
+        private System.Windows.Forms.TextBox Copyright;
+        private System.Windows.Forms.TextBox DateTimeEndTxtBox;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.DateTimePicker dtEnd;
+        private TextBox BedTimeTxtBox;
+        private DateTimePicker startTime;
+        private DateTimePicker bedTime;
+        private DateTimePicker endTime;
+    }
+}
+
---
+++ OhioHealth/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace OhioHealth
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OKBtn_Click(object sender, EventArgs e)
+        {
+            DateTime t1 = new DateTime(dtStart.Value.Year, dtStart.Value.Month, dtStart.Value.Day, 17, 0, 0);
+            DateTime t2 = new DateTime(dtEnd.Value.Year, dtEnd.Value.Month, (dtStart.Value.Day + 1), 4, 0, 0);
+            DateTime tMidnight = new DateTime(dtEnd.Value.Year, dtEnd.Value.Month, (dtStart.Value.Day + 1), 0, 0, 0);
+            DateTime startTimeTrue = new DateTime(dtStart.Value.Year, dtStart.Value.Month, dtStart.Value.Day, startTime.Value.Hour, startTime.Value.Minute, 0);
+            DateTime bedTimeTrue = new DateTime(dtStart.Value.Year, dtStart.Value.Month, dtStart.Value.Day, bedTime.Value.Hour, bedTime.Value.Minute, 0);
+            DateTime bedTimeNext = new DateTime(dtStart.Value.Year, dtStart.Value.Month, (dtStart.Value.Day + 1), bedTime.Value.Hour, bedTime.Value.Minute, 0);
+            DateTime endTimeTrue = new DateTime(dtEnd.Value.Year, dtEnd.Value.Month, dtEnd.Value.Day, endTime.Value.Hour, endTime.Value.Minute, 0);
+
+            if (endTimeTrue >= startTimeTrue)//confirm date End is after date start
+            {
+                if (startTimeTrue >= t1)//confirm start time is no earlier than 5pm
+                {
+                    DateTime bed;
+                    if (bedTime.Value >= startTime.Value)
+                    {
+                        bed = bedTimeTrue;
+                    }
+                    else
+                    {
+                        bed = bedTimeNext;
+                    }
+
+
+                    if (endTimeTrue <= t2)//confirm end time is no later than 4am
+                    {
+                        //ALL DATA HERE
+                        //CalculateData();//method for calculating $$$
+
+                        int startCur;
+                        int bedCur;
+                        int endCur;
+                        int finalCur;                                              
+
+                        if (bed < tMidnight && endTimeTrue >= tMidnight) //---check to see if bedtime is before midnight
+                                                       //---if bedtime is earlier than midnight calculate time from bedtime to midnight   
+                        {
+                            //$12/hr start time to bed time
+                            //Bedtime - startTime = #hours * 12  
+                            var startHrs = (int)Math.Ceiling((bed - startTimeTrue).TotalHours);
+                            startCur = (startHrs * 12);
+
+                            //$8/hr bed time to midnight
+                            //midnightTime - Bedtime = #hours * $8
+                            var bedHrs = (int)Math.Ceiling((tMidnight - bed).TotalHours);
+                            bedCur = ((Math.Max(0,bedHrs)) * 8);
+
+                            //$16/hr midnight to endtime
+                            //endTime - midnightTime = #hours * 16
+                            var endHrs = (int)Math.Ceiling((endTimeTrue - tMidnight).TotalHours);
+                            endCur = ((Math.Max(0,endHrs)) * 16);
+                                                       
+                            finalCur = startCur + bedCur + endCur;
+
+                            MessageBox.Show("$" + finalCur.ToString());
+                        }//closing (bed < tMidnight)                    
+
+                        else if (bed < tMidnight && endTimeTrue < tMidnight) //if bed time is less than midnight and end time is less than midnight
+                        {
+                            //$12/hr start time to bed time
+                            //Bedtime - startTime = #hours * 12  
+                            var startHrs = (int)Math.Ceiling((bed - startTimeTrue).TotalHours);
+                            startCur = (startHrs * 12);
+
+                            //$8/hr bed time to midnight
+                            //midnightTime - Bedtime = #hours * $8
+                            var bedHrs = (int)Math.Ceiling((endTimeTrue - bed).TotalHours);
+                            bedCur = ((Math.Max(0, bedHrs)) * 8);
+
+                            finalCur = startCur + bedCur;
+                            MessageBox.Show("$" + finalCur.ToString());
+                        }
+                        else if (bed >= tMidnight) //---check to see if bedtime is midnight or later 
+                                                             //---if bedtime is later than midnight
+                        {
+                            //$12/hr start time to midnight
+                            //midnightTime - startTime = #hours * 12
+                            var startHrs = (int)Math.Ceiling((tMidnight - startTimeTrue).TotalHours);
+                            startCur = (startHrs * 12);
+
+                            //$16/hr midnight to endtime
+                            //endTime - midnightTime = #hours * 16
+                            var hrs3 = (int)Math.Ceiling((endTimeTrue - bed).TotalHours);
+                            endCur = ((Math.Max(0,hrs3)) * 16);
+
+                            finalCur = startCur + endCur;
+                            MessageBox.Show("$"+finalCur.ToString());
+                        }//closing (bed >= tMidnight)
+
+                        //*****no fractions
+
+                        //but there's a possibility they babysit and don't put the baby to bed -- end babysitting before bed time
+                        //maybe create a checkbox if didn't put the baby to bed
+
+                        this.Hide();
+                        //MessageBox.Show(finalCur.ToString());
+                        Application.Exit();
+                    }//if (endTime <= t2) closing
+
+                    else if (endTimeTrue > t2)//end time is later than 4am
+                    {
+                        MessageBox.Show("Change End Time to 4am or earlier.");
+                    }//else if (endTime > t2) closing
+                }// if (startTime.Value >= t1) closing
+
+                else if (dtStart.Value > t1)//start time is earlier than 5pm
+                {
+                    MessageBox.Show("Change Start Time to 5pm or later.");
+                }//else if (startTime.Value > t1) closing
+
+                else
+                {
+                    MessageBox.Show("ERROR!");
+                    this.Hide();
+                    Application.Exit();
+                }//else closing
+            }//if (dateEnd.Value >= dateStart.Value) closing
+
+            else
+            {
+                MessageBox.Show("End Date is before Start Date.");
+            }//else closing
+
+
+
+
+        }//OKBtn_Click closing
+
+
+
+
+        //method for calculating $$$
+        private void CalculateData()
+        {
+            
+
+
+
+
+
+        }//CalculateData closing
+    }//Form 1 closing
+}//OhioHealth Namespace closing
